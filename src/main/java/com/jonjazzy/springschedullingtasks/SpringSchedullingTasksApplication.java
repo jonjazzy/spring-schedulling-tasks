bbb1ad56af02d62adcf03b9d1d998d3de4062a46
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-29 14:11:04 -0600

    spring-schedulling-tasks - My First Commit

---
+++ src/main/java/com/jonjazzy/springschedullingtasks/SpringSchedullingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.springschedullingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedullingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedullingTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonjazzy/springschedullingtasks/SpringSchedullingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.springschedullingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedullingTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
